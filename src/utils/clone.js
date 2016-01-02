019111f9966938d12d2a182a974b4db2f234cb44
Mark Finger <markfinger@gmail.com>
2016-01-02 15:10:14 +1100

    Docs

--- src/utils/clone.js
+++ src/utils/clone.js
@@ -3,8 +3,8 @@ import {forEach} from 'lodash/collection';
 import {startsWith} from 'lodash/string';
 
 /**
- * Deeply clones an object, but omits any properties that begin with
- * an underscore.
+ * Deeply clones an object, but omits any properties with a name
+ * beginning with an underscore.
  *
  * Babel likes to annotate ASTs with "private" properties that are used
  * during traversal, so this method is useful for:
@@ -13,11 +13,11 @@ import {startsWith} from 'lodash/string';
  * - Ensuring that ASTs are serializable (babel's "private" properties
  *   often contain circular structures that cannot be serialized)
  *
- * Be aware that any circular structures in properties that do not begin
- * with an underscore will cause this function to blow either your memory
- * or stack limits. Unless you are handling an AST, you should probably
- * use a deep cloning method that is designed to handle circular
- * structures.
+ * Be aware that any circular structures in properties with a name that
+ * does not begin with an underscore will cause this function to blow
+ * either your memory or stack limits. Unless you are handling a babel
+ * AST, you should probably use a deep cloning method that is designed
+ * to handle circular structures.
  *
  * @param {Object} obj
  * @returns {Object}
