8b312878d469af542619735193f7762a53874e98
Mark Finger <markfinger@gmail.com>
2016-01-03 19:10:59 +1100

    Enabling the babel last dependency analyser to use the cache

--- src/dependencies/babel_ast_dependency_analyzer.js
+++ src/dependencies/babel_ast_dependency_analyzer.js
@@ -4,49 +4,74 @@ import traverse from 'babel-traverse';
 import * as types from 'babel-types';
 import {cloneDeepOmitPrivateProps} from '../utils/clone';
 
+export function generateBabelAstDependencyAnalyzerCacheKey(file) {
+  return {
+    key: file,
+    namespace: __filename,
+    packageDependencies: ['babel-traverse']
+  }
+}
+
 export function createBabelAstDependencyAnalyzer() {
   return function babelAstDependencyAnalyzer(options, pipeline, cb) {
-    const {ast} = options;
+    const {ast, file} = options;
+    const {cache} = pipeline;
 
     if (!isObject(ast)) {
       return cb(new Error(`An \`ast\` option must be provided: ${JSON.stringify(options)}`))
     }
 
-    const dependencies = [];
-    const errors = [];
+    // TODO: add `file` type check as well
 
-    // Ensure that any mutations to the traversed AST are not applied
-    // to the provided AST
-    const clonedAst = cloneDeepOmitPrivateProps(ast);
+    const cacheKey = generateBabelAstDependencyAnalyzerCacheKey(file);
 
-    traverse(clonedAst, {
-      ImportDeclaration(node) {
-        dependencies.push(node.node.source.value);
-      },
-      CallExpression(node) {
-        const callNode = node.node;
-        if (callNode.callee.name === 'require') {
-          const arg = callNode.arguments[0];
-          if (types.isLiteral(arg)) {
-            dependencies.push(arg.value);
-          } else {
-            let err = `Non-literal (${arg.type}) passed to \`require\` call`;
-            if (arg.loc && arg.loc.start) {
-              err += ` at line ${arg.loc.start.line}, column ${arg.loc.start.column}`
+    cache.get(cacheKey, (err, cachedDeps) => {
+      if (err) return cb(err);
+
+      if (isObject(cachedDeps)) {
+        return cb(null, cachedDeps);
+      }
+
+      const dependencies = [];
+      const errors = [];
+
+      // Ensure that any mutations to the traversed AST are not applied
+      // to the provided AST
+      const clonedAst = cloneDeepOmitPrivateProps(ast);
+
+      traverse(clonedAst, {
+        ImportDeclaration(node) {
+          dependencies.push(node.node.source.value);
+        },
+        CallExpression(node) {
+          const callNode = node.node;
+          if (callNode.callee.name === 'require') {
+            const arg = callNode.arguments[0];
+            if (types.isLiteral(arg)) {
+              dependencies.push(arg.value);
+            } else {
+              let err = `Non-literal (${arg.type}) passed to \`require\` call`;
+              if (arg.loc && arg.loc.start) {
+                err += ` at line ${arg.loc.start.line}, column ${arg.loc.start.column}`
+              }
+              errors.push(err);
             }
-            errors.push(err);
           }
         }
+      });
+
+      if (errors.length) {
+        return cb(new Error(errors.join('\n\n')));
       }
+
+      // Ensure that dependencies are only identified once
+      const uniqueDependencies = uniq(dependencies);
+
+      cache.set(cacheKey, uniqueDependencies, (err) => {
+        if (err) return cb(err);
+
+        cb(null, uniqueDependencies);
+      });
     });
-
-    if (errors.length) {
-      const err = new Error(errors.join('\n\n'));
-      return cb(err);
-    }
-
-    // Ensure that dependencies are only identified once
-    const uniqueDependencies = uniq(dependencies);
-    cb(null, uniqueDependencies);
   };
 }
