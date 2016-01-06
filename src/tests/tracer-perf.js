30fb87755d37d1c976025aab79b25566b1b28ce4
Mark Finger <markfinger@gmail.com>
2016-01-06 17:45:56 +1100

    Perf test improvements

--- src/tests/tracer-perf.js
+++ src/tests/tracer-perf.js
@@ -6,90 +6,77 @@ import {assert} from '../utils/assert';
 import {browserResolver} from '../dependencies/browser_resolve';
 import {analyzeBabelAstDependencies} from '../dependencies/babel_ast_dependency_analyzer';
 import {createWorkerFarm} from '../workers/worker_farm';
-import {createFileCache} from '../kv-file-cache';
+import {createFileCache, createMockCache} from '../kv-file-cache';
 
-function createBlobCache() {
-  const cacheFile = path.join(__dirname, 'cache.json');
-  let cache = Object.create(null);
-
-  let _isReady = false;
-  const _onReady = [];
-  function onReady(cb) {
-    if (_isReady) {
-      cb();
-    } else {
-      _onReady.push(cb);
-    }
-  }
-
-  fs.readFile(cacheFile, 'utf8', (err, text) => {
-    _isReady = true;
-
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-    cache = JSON.parse(text);
-    _onReady.forEach(cb => cb());
-  });
-
-  process.on('exit', () => {
-    fs.writeFileSync(cacheFile, JSON.stringify(cache));
-  });
-
-  return {
-    get(options, cb) {
-      onReady(() => {
-        if (cache[options.key]) {
-          return cb(null, cache[options.key]);
-        }
-        cb(null, null);
-      });
-    },
-    set(options, value, cb) {
-      onReady(() => {
-        cache[options.key] = value;
-        cb(null);
-      });
-    }
-  }
-}
-
-function trace(cb) {
+function trace(caches, cb) {
   const tree = Object.create(null);
 
-  function traceFile(file, cb) {
-    tree[file] = [];
+  function getAst(file, cb) {
+    caches.ast.get(file, (err, data) => {
+      if (err || data) return cb(err, data);
 
-    async.waterfall([
-      (cb) => fs.readFile(file, 'utf8', cb),
-      (text, cb) => {
+      fs.readFile(file, 'utf8', (err, text) => {
+        if (err) return cb(err);
+
         let ast;
         try {
           ast = babylon.parse(text, {sourceType: 'module'})
         } catch(err) {
           return cb(err);
         }
+
+        caches.ast.set(file, ast);
         cb(null, ast);
-      },
-      (ast, cb) => {
+      });
+    });
+  }
+
+  function getDependencies(file, cb) {
+    caches.dependencies.get(file, (err, data) => {
+      if (err || data) return cb(err, data);
+
+      getAst(file, (err, ast) => {
+        if (err) return cb(err);
+
         let dependencies;
         try {
           dependencies = analyzeBabelAstDependencies(ast);
         } catch(err) {
           return cb(err);
         }
+
+        caches.dependencies.set(file, dependencies);
         cb(null, dependencies);
-      },
-      (deps, cb) => async.map(
-        deps,
-        (dependency, cb) => {
-          browserResolver({dependency, basedir: path.dirname(file)}, cb);
-        },
-        cb
-      )
-    ], (err, resolved) => {
+      });
+    });
+  }
+
+  function getResolvedDependencies(file, cb) {
+    caches.resolver.get(file, (err, data) => {
+      if (err || data) return cb(err, data);
+
+      getDependencies(file, (err, deps) => {
+        if (err) return cb(err);
+
+        async.map(
+          deps,
+          (dependency, cb) => {
+            browserResolver({dependency, basedir: path.dirname(file)}, cb);
+          },
+          (err, resolved) => {
+            if (err) return cb(err);
+            caches.resolver.set(file, resolved);
+            cb(null, resolved);
+          }
+        );
+      });
+    });
+  }
+
+  function traceFile(file, cb) {
+    tree[file] = [];
+
+    getResolvedDependencies(file, (err, resolved) => {
       if (err) {
         err.message = `File: ${file}\n\n${err.message}`;
         return cb(err);
@@ -118,46 +105,44 @@ function trace(cb) {
   });
 }
 
+function createMockCaches() {
+  const mockCache = createMockCache();
+  return {
+    ast: mockCache,
+    dependencies: mockCache,
+    resolver: mockCache
+  }
+}
+
+function createFileCaches() {
+  return {
+    ast: createFileCache({dirname: path.join(__dirname, 'ast_cache')}),
+    dependencies: createFileCache({dirname: path.join(__dirname, 'dependency_cache')}),
+    resolver: createFileCache({dirname: path.join(__dirname, 'resolver_cache')})
+  }
+}
+
 module.exports = function tracerPerf(cb) {
   const start = (new Date).getTime();
 
-  // normal
-  trace((err, tree) => {
+  //trace(createMockCaches(), (err, tree) => {
+  //  assert.isNull(err);
+  //  assert.isObject(tree);
+  //
+  //  const end = (new Date).getTime() - start;
+  //  console.log(`\n\nTraced ${Object.keys(tree).length} records in ${end}ms with mock caches`);
+  //
+  //  cb();
+  //});
+
+
+  trace(createFileCaches(), (err, tree) => {
     assert.isNull(err);
     assert.isObject(tree);
 
     const end = (new Date).getTime() - start;
-    console.log(`\n\nTraced ${Object.keys(tree).length} records in ${end}ms`);
+    console.log(`\n\nTraced ${Object.keys(tree).length} records in ${end}ms with file caches`);
 
     cb();
   });
-
-
-  //// persistent cache
-  //const cachePipeline = createPipeline({
-  //  cache: createPersistentCache({dirname: __dirname + '/test-cache'})
-  //});
-  //trace(cachePipeline, (err, tree) => {
-  //  assert.isNull(err);
-  //  assert.isObject(tree);
-  //
-  //  const end = (new Date).getTime() - start;
-  //  console.log(`\n\nTraced ${Object.keys(tree).length} records in ${end}ms`);
-  //
-  //  cb();
-  //});
-
-  // blob cache
-  //const cachePipeline = createPipeline({
-  //  cache: createBlobCache()
-  //});
-  //trace(cachePipeline, (err, tree) => {
-  //  assert.isNull(err);
-  //  assert.isObject(tree);
-  //
-  //  const end = (new Date).getTime() - start;
-  //  console.log(`\n\nTraced ${Object.keys(tree).length} records in ${end}ms`);
-  //
-  //  cb();
-  //});
 };
