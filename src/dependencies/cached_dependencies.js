b3110f3ed406885abd630da16a063f715bf6b061
Mark Finger <markfinger@gmail.com>
2016-01-09 13:42:12 +1100

    Code cleanup

--- src/dependencies/cached_dependencies.js
+++ src/dependencies/cached_dependencies.js
@@ -84,61 +84,60 @@ export function getAggressivelyCachedResolvedDependencies({cache, key, file, dep
 }
 
 export function getCachedResolvedDependencies({cache, key, file, dependencyIdentifierCache, astCache}, cb) {
-  getCachedDependencyIdentifiers(
-    {cache: dependencyIdentifierCache, key, file, astCache},
-    (err, identifiers) => {
-      if (err) return cb(err);
+  function resolveIdentifiers(err, identifiers) {
+    if (err) return cb(err);
 
-      const pathIdentifiers = [];
-      const packageIdentifiers = [];
-      identifiers.forEach(identifier => {
-        if (identifier[0] !== '.' && !path.isAbsolute(identifier)) {
-          packageIdentifiers.push(identifier);
-        } else {
-          pathIdentifiers.push(identifier);
-        }
-      });
+    const pathIdentifiers = [];
+    const packageIdentifiers = [];
+    identifiers.forEach(identifier => {
+      if (identifier[0] !== '.' && !path.isAbsolute(identifier)) {
+        packageIdentifiers.push(identifier);
+      } else {
+        pathIdentifiers.push(identifier);
+      }
+    });
 
-      async.parallel([
-        // If a dependency identifier is relative (./ ../) or absolute (/), there are
-        // edge-cases where caching the resolved path may produce the wrong result.
-        // For example: an identifier, "./foo" may resolve to either a "./foo.js" or
-        // or "./foo/index.js". Detecting these cases is problematic, so we avoid the
-        // problem by ensuring that the resolver always inspects the file system for
-        // path-based identifiers
-        (cb) => {
+    async.parallel([
+      // If a dependency identifier is relative (./ ../) or absolute (/), there are
+      // edge-cases where caching the resolved path may produce the wrong result.
+      // For example: an identifier, "./foo" may resolve to either a "./foo.js" or
+      // or "./foo/index.js". Detecting these cases is problematic, so we avoid the
+      // problem by ensuring that the resolver always inspects the file system for
+      // path-based identifiers
+      (cb) => {
+        async.map(
+          pathIdentifiers,
+          (identifier, cb) => browserResolver(identifier, path.dirname(file), cb),
+          (err, resolvedIdentifiers) => {
+            if (err) return cb(err);
+
+            cb(null, zip(pathIdentifiers, resolvedIdentifiers));
+          }
+        )
+      },
+      // If a dependency identifier refers to a package (eg: is not a path-based identifier),
+      // we can cache the resolved path and leave higher levels to perform cache invalidation
+      (cb) => {
+        function compute(cb) {
           async.map(
-            pathIdentifiers,
+            packageIdentifiers,
             (identifier, cb) => browserResolver(identifier, path.dirname(file), cb),
             (err, resolvedIdentifiers) => {
               if (err) return cb(err);
 
-              cb(null, zip(pathIdentifiers, resolvedIdentifiers));
+              cb(null, zip(packageIdentifiers, resolvedIdentifiers));
             }
           )
-        },
-        // If a dependency identifier refers to a package (eg: is not a path-based identifier),
-        // we can cache the resolved path and leave higher levels to perform cache invalidation
-        (cb) => {
-          function compute(cb) {
-            async.map(
-              packageIdentifiers,
-              (identifier, cb) => browserResolver(identifier, path.dirname(file), cb),
-              (err, resolvedIdentifiers) => {
-                if (err) return cb(err);
-
-                cb(null, zip(packageIdentifiers, resolvedIdentifiers));
-              }
-            )
-          }
-
-          getCachedData({cache, key, compute}, cb);
         }
-      ], (err, data) => {
-        if (err) return cb(err);
 
-        cb(null, flatten(data));
-      });
-    }
-  );
+        getCachedData({cache, key, compute}, cb);
+      }
+    ], (err, data) => {
+      if (err) return cb(err);
+
+      cb(null, flatten(data));
+    });
+  }
+
+  getCachedDependencyIdentifiers({cache: dependencyIdentifierCache, key, file, astCache}, resolveIdentifiers);
 }
