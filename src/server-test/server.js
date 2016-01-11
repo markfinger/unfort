c89b5db431da6fd88eaa57f817de30d67abec4fb
Mark Finger <markfinger@gmail.com>
2016-01-11 14:55:18 +1100

    got hot transform to the point of failing only because of no `module.hot` api

--- src/server-test/server.js
+++ src/server-test/server.js
@@ -3,11 +3,12 @@ import path from 'path';
 import async from 'async';
 import sourceMapSupport from 'source-map-support';
 import express from 'express';
+import * as babel from 'babel-core';
 import {startsWith} from 'lodash/string';
 import {hashNpmDependencyTree} from '../hash-npm-dependency-tree';
 import {
-  getAggressivelyCachedResolvedDependencies,
-  getCachedResolvedDependencies
+  getAggressivelyCachedResolvedDependencies, getCachedResolvedDependencies, getCachedAst,
+  getCachedDependencyIdentifiers
 } from '../dependencies/cached_dependencies';
 import {createMockCaches, createFileCaches} from '../tests/tracer_perf';
 
@@ -19,6 +20,7 @@ const entryFile = path.join(sourceRoot, 'src', 'server-test', 'entry.js');
 
 const tree = Object.create(null);
 const files = Object.create(null);
+const transformedFiles = Object.create(null);
 
 const start = (new Date()).getTime();
 
@@ -102,39 +104,21 @@ function startServer() {
       return res.end(files[src]);
     }
 
-    fs.readFile(src, 'utf8', (err, text) => {
-      if (err) return res.status(500).send(`File: ${src}\n\n${err.stack}`);
-
-      if (!startsWith(src, rootNodeModules)) {
-        const babel = require('babel-core');
-        const file = babel.transform(text, {
-          plugins: [
-            ['react-transform', {
-              "transforms": [{
-                "transform": "react-transform-hmr"
-              }]
-            }]
-          ],
-          "presets": [
-            "es2015",
-            "react"
-          ]
-        });
-        text = file.code;
+    if (transformedFiles[src]) {
+      if (!files[src]) {
+        files[src] = wrapCommonJSModule({file: src, dependencies: tree[src], code: transformedFiles[src]});
       }
 
-      const dependencies = JSON.stringify(tree[src]);
+      res.end(files[src]);
+    } else {
+      fs.readFile(src, 'utf8', (err, text) => {
+        if (err) return res.status(500).send(`File: ${src}\n\n${err.stack}`);
 
-      text = (
-        `__modules[${JSON.stringify(src)}] = [${dependencies}, function(module, exports, require, process, global){\n` +
-        text +
-        '\n}];'
-      );
+        files[src] = wrapCommonJSModule({file: src, dependencies: tree[src], code: text});
 
-      files[src] = text;
-
-      return res.end(text);
-    });
+        res.end(files[src]);
+      });
+    }
   });
 
   app.get('/tree', (req, res) => {
@@ -146,69 +130,116 @@ function startServer() {
   });
 }
 
+function wrapCommonJSModule({code, file, dependencies}) {
+  dependencies = JSON.stringify(dependencies);
+  return (
+    `__modules[${JSON.stringify(file)}] = [${dependencies}, function(module, exports, require, process, global){\n` +
+    code +
+    '\n}];'
+  );
+}
+
 function traceFile(file, tree, caches, cb) {
   tree[file] = [];
 
+  function onTraceComplete(err) {
+    if (err) {
+      err.message = `Error in file: ${file}\n\n${err.message}`;
+      return cb(err);
+    }
+    cb(null);
+  }
+
+  console.log(file)
   fs.stat(file, (err, stat) => {
     if (err) return cb(err);
 
-    getResolvedDependencies(file, stat, caches, (err, resolved) => {
-      if (err) {
-        err.message = `File: ${file}\n\n${err.message}`;
-        return cb(err);
+    const key = file + stat.mtime.getTime();
+
+    function getAst(cb) {
+      if (startsWith(file, rootNodeModules)) {
+        return getCachedAst({cache: caches.ast, key, file}, cb);
       }
+
+      babel.transformFile(
+        file,
+        {
+          plugins: [
+            //'transform-object-rest-spread',
+            //'transform-react-jsx',
+            ['react-transform', {
+              transforms: [{
+                transform: 'react-transform-hmr',
+                imports: ['react'],
+                locals: ['module']
+              }]
+            }]
+          ]
+          //,
+          //'presets': ['es2015']
+        },
+        (err, transformed) => {
+          if (err) return cb(err);
+
+          transformedFiles[file] = transformed.code;
+
+          cb(null, transformed.ast);
+        }
+      );
+    }
+
+    function getDependencyIdentifiers(cb) {
+      getCachedDependencyIdentifiers(
+        {cache: caches.dependencyIdentifiers, key, file, getAst},
+        cb
+      )
+    }
+
+    function onDepsResolved(err, resolved) {
+      if (err) return onTraceComplete(err);
 
       tree[file] = resolved;
 
-      const untracedFiles = [];
+      const untracedFiles = resolved
+        .filter(dep => !tree[dep[1]])
+        .map(dep => dep[1]);
 
-      resolved.forEach(dep => {
-        const filename = dep[1];
-        if (!tree[filename]) {
-          untracedFiles.push(filename);
-        }
+      if (!untracedFiles.length) {
+        return onTraceComplete(null);
+      }
+
+      untracedFiles.forEach(file => {
+        tree[file] = {};
       });
 
-      if (untracedFiles.length) {
-        untracedFiles.forEach(file => {
-          tree[file] = {};
-        });
+      async.map(
+        untracedFiles,
+        (file, cb) => traceFile(file, tree, caches, cb),
+        onTraceComplete
+      );
+    }
 
-        async.map(
-          untracedFiles,
-          (file, cb) => traceFile(file, tree, caches, cb),
-          cb
-        );
-      } else {
-        cb(null);
-      }
-    });
+    // If the file is within the root node_modules, we can aggressively
+    // cache its resolved dependencies
+    if (startsWith(file, rootNodeModules)) {
+      getAggressivelyCachedResolvedDependencies(
+        {cache: caches.resolvedDependencies, key, file, getDependencyIdentifiers},
+        onDepsResolved
+      );
+    } else {
+      getCachedResolvedDependencies(
+        {cache: caches.resolvedDependencies, key, file, getDependencyIdentifiers},
+        onDepsResolved
+      );
+    }
   });
 }
 
-function getResolvedDependencies(file, stat, caches, cb) {
-  const key = file + stat.mtime.getTime();
-
-  const options = {
-    cache: caches.resolvedDependencies,
-    astCache: caches.ast,
-    dependencyIdentifierCache: caches.dependencyIdentifiers,
-    key,
-    file
-  };
-
-  // If the file is within the root node_modules, we can aggressively
-  // cache its resolved dependencies
-  if (startsWith(file, rootNodeModules)) {
-    getAggressivelyCachedResolvedDependencies(options, cb);
-  } else {
-    getCachedResolvedDependencies(options, cb);
-  }
-}
-
 hashNpmDependencyTree(process.cwd(), (err, hash) => {
-  const caches = createFileCaches(hash);
-  //const caches = createMockCaches();
+  if (err) throw err;
+
+  //const caches = createFileCaches(hash);
+  const caches = createMockCaches();
   traceFile(entryFile, tree, caches, (err) => {
     if (err) throw err;
     const end = (new Date()).getTime() - start;
