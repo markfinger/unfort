0549daa135405da9f58ea021e1cb15dca92acc33
Mark Finger <markfinger@gmail.com>
2016-02-18 08:30:15 +1100

    Docs

--- src/unfort/utils.js
+++ src/unfort/utils.js
@@ -4,18 +4,32 @@ import babelCodeFrame from 'babel-code-frame';
 import chalk from 'chalk';
 import {includes} from 'lodash/collection';
 
-// We need to rebuild source maps for js files such that it
-// reflects the call to our module runtime. Unfortunately, source
-// maps are slow to both consume and generate. However, given
-// that we know our call to the runtime only consumes one line,
-// we can take advantage of the line offset character in source
-// maps to simply offset everything.
-//
-// For context, source maps are encoded such that that semicolons
-// are used to indicate line offset. Hence, we can just prepend
-// a semi-colon to achieve the desired effect
+/**
+ * We need to rebuild source maps for js files such that it
+ * reflects the call to our module runtime. Unfortunately, source
+ * maps are slow to both consume and generate. However, given
+ * that we know our call to the runtime only consumes one line,
+ * we can take advantage of the line offset character in source
+ * maps to simply offset everything.
+ *
+ * For context, source maps are encoded such that that semicolons
+ * are used to indicate line offset. Hence, we can just prepend
+ * a semi-colon to achieve the desired effect
+ *
+ * @type {String}
+ */
 export const JS_MODULE_SOURCE_MAP_LINE_OFFSET = ';';
 
+/**
+ * Produces a string that can be used to inject a module definition
+ * into the bootstrap runtime
+ *
+ * @param name
+ * @param deps
+ * @param hash
+ * @param code
+ * @returns {String}
+ */
 export function createJSModuleDefinition({name, deps, hash, code}) {
   const lines = [
     `__modules.defineModule({name: ${JSON.stringify(name)}, deps: ${JSON.stringify(deps)}, hash: ${JSON.stringify(hash)}, factory: function(module, exports, require, process, global) {`,
@@ -26,9 +40,13 @@ export function createJSModuleDefinition({name, deps, hash, code}) {
   return lines.join('\n');
 }
 
+/**
+ * Produces a description of a record that the hot runtime can consume
+ *
+ * @param {Record} record
+ * @returns {Object}
+ */
 export function createRecordDescription(record) {
-  // Produce a description of a record that the hot runtime
-  // can consume
   return {
     name: record.name,
     hash: record.data.hash,
@@ -37,6 +55,18 @@ export function createRecordDescription(record) {
   };
 }
 
+/**
+ * Returns a stream that can pipe a record's content.
+ *
+ * If the record represents a text file, it emits the Record's
+ * `code` and, if available, the `sourceMapAnnotation`.
+ *
+ * If the record is not a text file, it simply pipes directly
+ * from the filesystem.
+ *
+ * @param {Record} record
+ * @returns {stream.Readable}
+ */
 export function createRecordContentStream(record) {
   if (!record.data.isTextFile) {
     return fs.createReadStream(record.name);
@@ -54,6 +84,12 @@ export function createRecordContentStream(record) {
   return stream;
 }
 
+/**
+ * Returns a stream that can pipe a record's source map
+ *
+ * @param {Record} record
+ * @returns {stream.Readable}
+ */
 export function createRecordSourceMapStream(record) {
   const stream = new Readable();
   stream.push(record.data.sourceMap);
@@ -61,6 +97,16 @@ export function createRecordSourceMapStream(record) {
   return stream;
 }
 
+/**
+ * Given an Error object, produces a textual description.
+ *
+ * Accepts an optional path to a file, which will be used to
+ * provide extra context for the error.
+ *
+ * @param {Error} err
+ * @param {String} [file]
+ * @returns {string}
+ */
 export function describeError(err, file) {
   const lines = [];
 
@@ -97,6 +143,13 @@ export function describeError(err, file) {
   return lines.join('\n');
 }
 
+/**
+ * Given an Array of errors (or error objects from the graph), produce a
+ * a textual description appropriate for logging
+ *
+ * @param {Array} errors
+ * @returns {String}
+ */
 export function describeErrorList(errors) {
   return errors
     .map(obj => {
