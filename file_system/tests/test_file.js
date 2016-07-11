5ad083d32c03f7ab343ae56a6f9b5d230d6925f3
Mark Finger <markfinger@gmail.com>
2016-07-11 21:02:33 +1000

    Test coverage

--- file_system/tests/test_file.js
+++ file_system/tests/test_file.js
@@ -50,13 +50,31 @@ describe('file_system/file', () => {
       const stat = fs.statSync(__filename);
       file.setStat(stat);
       return file.getStat()
-        .then(_stat => {
-          assert.strictEqual(_stat, stat);
-          return file.getModifiedTime()
-            .then(modifiedTime => {
-              assert.equal(modifiedTime, stat.mtime.getTime());
-            });
-        });
+        .then(_stat => assert.strictEqual(_stat, stat));
+    });
+    it('should allow isFile to be manually set', () => {
+      const file = new File('___NON_EXISTENT_FILE__');
+      file.setIsFile('test');
+      return file.getIsFile()
+        .then(isFile => assert.equal(isFile, 'test'));
+    });
+    it('should allow modifiedTime to be manually set', () => {
+      const file = new File('___NON_EXISTENT_FILE__');
+      file.setModifiedTime('test');
+      return file.getModifiedTime()
+        .then(modifiedTime => assert.equal(modifiedTime, 'test'));
+    });
+    it('should allow text to be manually set', () => {
+      const file = new File('___NON_EXISTENT_FILE__');
+      file.setText('test');
+      return file.getText()
+        .then(text => assert.equal(text, 'test'));
+    });
+    it('should allow text hash to be manually set', () => {
+      const file = new File('___NON_EXISTENT_FILE__');
+      file.setTextHash('test');
+      return file.getTextHash()
+        .then(textHash => assert.equal(textHash, 'test'));
     });
     describe('file system interactions', () => {
       it('should create an object that lazily evaluates text files and preserves the value', () => {
