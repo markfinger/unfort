ab1c82ade0d2bf5f618521e5f49bd30bec038b62
Mark Finger <markfinger@gmail.com>
2016-08-19 10:10:38 +1000

    Remove typings

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/mkdirp/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/sqlite3/index.d.ts" />
-/// <reference path="globals/tmp/index.d.ts" />
